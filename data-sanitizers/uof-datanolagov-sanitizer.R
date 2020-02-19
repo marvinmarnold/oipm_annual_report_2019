b64ca7db6c82ed0e9352483af2d41bfca5507b99
Marvin Arnold <marvin@geeky.rocks>
2020-02-18 23:11:12 -0600

    Updates

--- data-sanitizers/uof-datanolagov-sanitizer.R
+++
@@ -1,9 +0,0 @@
-check.vars(c("UOF.CSV.DIRTY", "UOF.CSV.SANITIZED", "CSV_SEP"))
-
-uof.dirty <- read.csv(UOF.CSV.DIRTY, stringsAsFactors = FALSE, sep = ",", na.strings=c("","NA"))
-
-uof.dirty <- uof.dirty %>% mutate(
-  Year.of.Record = substr(PIB.File.Number, 4, 7)
-)
-
-write.table(uof.dirty, UOF.CSV.SANITIZED, sep = CSV_SEP)
